9311579addf486182830d8198ef5f411bac7822c
Miguel de Dios Matias <tres.14159@gmail.com>
2018-12-10 11:21:40 +0100

    Fixed re.split (for python 3.7) in get_hdf5_version

--- setup.py
+++ setup.py
@@ -347,11 +347,11 @@ def get_hdf5_version(headername):
     with open(headername) as fd:
         for line in fd:
             if 'H5_VERS_MAJOR' in line:
-                major_version = int(re.split("\s*", line)[2])
+                major_version = int(re.split("\s+", line)[2])
             if 'H5_VERS_MINOR' in line:
-                minor_version = int(re.split("\s*", line)[2])
+                minor_version = int(re.split("\s+", line)[2])
             if 'H5_VERS_RELEASE' in line:
-                release_version = int(re.split("\s*", line)[2])
+                release_version = int(re.split("\s+", line)[2])
             if (major_version != -1 and minor_version != -1 and
                     release_version != -1):
                 break
